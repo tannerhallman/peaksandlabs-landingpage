6d001278b7adf747ff3234a688779c590d22631e
Tanner Hallman <tannerhallman@users.noreply.github.com>
2024-08-07 15:04:21 -0600

    feat: add speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import "./globals.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 interface Props {
   children: React.ReactNode; // <--- Added type annotation here
 }
@@ -29,6 +30,7 @@ export default function Layout({ children }: Props) {
       <body
         className={cn("antialiased", fontHeading.variable, fontBody.variable)}>
         <Analytics />
+        <SpeedInsights />
         <Header />
         {children}
         <Footer />
