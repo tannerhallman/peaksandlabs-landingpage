76877cca1b2459704184270417cd71daff515d80
Tanner Hallman <tannerhallman@users.noreply.github.com>
2024-08-07 15:00:15 -0600

    feat: added nextjs analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { cn } from "@/lib/utils";
 import "./globals.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import { Analytics } from "@vercel/analytics/react";
 interface Props {
   children: React.ReactNode; // <--- Added type annotation here
 }
@@ -27,6 +28,7 @@ export default function Layout({ children }: Props) {
     <html lang='en'>
       <body
         className={cn("antialiased", fontHeading.variable, fontBody.variable)}>
+        <Analytics />
         <Header />
         {children}
         <Footer />
